8f74d7a902db7a72ba83f5416090a486d12c1fdf
Rutwij <62652399+Rutwij12@users.noreply.github.com>
2023-07-27 08:56:52 +0100

    Commit Databases in app.js
    Sample code to connect written at top of file

--- app.js
+++ app.js
@@ -1,3 +1,8 @@
+    // let sqlQuery = ``; 
+    // connection.query(sqlQuery, function(err, result) {
+    //     if (err) throw err; 
+    // })
+
 const express = require("express");
 const bodyParser = require("body-parser");
 
@@ -6,6 +11,23 @@ const app = express();
 app.use(bodyParser.urlencoded({extended:true})); 
 app.use(express.static("Public"));
 
+const mysql = require('mysql');
+
+const connection = mysql.createConnection({
+    host: '127.0.0.1', 
+    port: '3306',
+    user: 'root', 
+    database: 'user_data', 
+    password: 'password'
+}); 
+
+connection.connect((error) => {
+    if (error) {
+        console.log(error);
+        return; 
+    }
+    console.log("Connection established successfully");
+});
 
 app.get("/", function(req, res) {
     res.sendFile(__dirname + "/index.html"); 
